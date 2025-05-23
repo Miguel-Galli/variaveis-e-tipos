21361dfb7925c08912322e36c56bf8facaff59fd
Miguel Guedes de Oliveira <miguelguedes.oli@gmail.com>
2025-05-22 21:03:39 -0300

    concluindo excercios de variaveis e tipos

--- src/Main.java
+++ src/Main.java
@@ -38,7 +38,7 @@ public class Main {
         System.out.println(fahrenheit);*/
 
         //cadastro de livro
-        char categoriaLivro;
+        /*char categoriaLivro;
         categoriaLivro = 'f';
         Livro livro = new Livro("O pequeno principe", "Antoine de Saint-exupéry", 96, 39.9, categoriaLivro);
         String saida = "Livro cadastrado: " + livro.titulo +
@@ -55,11 +55,64 @@ public class Main {
             System.out.println(saida + "Tecnologia");
         } else {
             System.out.println(saida + "Hisória");
+        }*/
+
+        //Classificação por Categoria
+        /*double preco = 150.0;
+
+        if (preco <= 50.0) {
+            System.out.println("Categoria do produto: Econômico");
+        } else if (preco > 50.0 && preco <= 200.0) {
+            System.out.println("Categoria do produto: Intermediário");
+        } else {
+            System.out.println("Categoria do produto: premium");
+        }*/
+
+        // Par ou Impar
+        /*int numero = 7;
+        int parOuImpar = numero % 2;
+
+        if (parOuImpar == 0){
+            System.out.println("Esse numero é par");
+        } else {
+            System.out.println("Esse numero é impar")
+            };*/
+
+        //Conversão de moeda
+        /*double valorEmReais = 451.50;
+        double taxaDeCambio = 5.25;
+        double valorEmDolar = valorEmReais / taxaDeCambio;
+        System.out.println("Valor em dolar: " + valorEmDolar);*/
+
+        //verificação de idade e escopo de variaveis
+        /*int idade = 17;
+        String mensagem;
+        if (idade >= 18) {
+            mensagem = "Você é maior de idade.";
+        } else {
+            mensagem = "Você é menor de idade.";
         }
 
+        System.out.println(mensagem);*/
 
+        //planejando uma viagem: consumo e autonomia
 
+        double consumoMedio = 12.5;
+        double capacidadeTanque = 40.0;
+        double quantidadeCombustivel = 20.0;
+        double distanciaViagem = 300.0;
 
+        double autonomiaMaxima = consumoMedio * capacidadeTanque;
+        double autonomiaAtual = consumoMedio * quantidadeCombustivel;
+
+        System.out.printf("Autonomia maxima do veiculo: %.1f Km%n", autonomiaMaxima);
+        System.out.printf("Autonomia atual do veiculo: %.1f Km%n", autonomiaAtual);
+
+        if (autonomiaAtual >= distanciaViagem) {
+            System.out.println("voce consiguirá completar sua viagem sem precisar abastecer o tanque.");
+        } else {
+            System.out.println("Abasteça antes de viajar");
+        }
 
 
     }
